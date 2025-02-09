1280f0363a471ec5ad42b4c7a94d4dbcaf9b35c3
romanpastu <roman.pastushenko.1997@gmail.com>
2025-02-09 12:53:32 +0100

    disable global caching and multi retrying in the react query router

--- front/src/lib/api/query.ts
+++ front/src/lib/api/query.ts
@@ -3,10 +3,10 @@ import { QueryClient } from '@tanstack/react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 60 * 1000, // 5 minutes
-      gcTime: 10 * 60 * 1000, // 10 minutes
+      staleTime: 0,
+      gcTime: 0,
       retry: 1,
-      refetchOnWindowFocus: false,
+      refetchOnWindowFocus: true,
     },
     mutations: {
       retry: 1,
